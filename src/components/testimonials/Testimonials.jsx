dd658e71e39768d9c8bc64244e866d3d06c47de0
Bobby Ton <bobbyt1997@gmail.com>
2022-08-20 12:54:57 -0500

    update testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -15,65 +15,44 @@ const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-      link: 'https://www.linkedin.com/in/uzairali19/',
-      name: 'Uzair Ali',
-      role: 'Full-Stack Web Developer. JavaScript | Rails | React | Redux. Improving open-source projects, one commit at a time.',
-      test: 'As a developer and a problem solver, I think Meri is a great collaborative partner to have. I met Meri in some basic javascript & react projects and since then she has drastically progressed in her understanding of the development process. She always has a professional environment and has good audio and video quality which makes it easier to communicate with her.',
+      link: 'https://linkedin.com/in/kenellorando/',
+      name: 'Kenneth Ellorando',
+      role: 'Cloud Engineer @ NextCapital',
+      test: "I have known Bobby for the past five years. Since the day we met, Bobby has remained laser-focused on his ultimate career goal of becoming a Software Developer. He shared with me his growing fascination with code since he was first exposed to it in high school. In college, Bobby was one of the very few people I considered a reliable study partner. Bobby's strength is his self-drive; he always did as much as he could on his own before turning to me or a professor for help. When doing group projects, I enjoyed having Bobby on my team, because he would always go beyond doing his share of the work. One night, Bobby and I spent twelve hours studying for a final and finishing a final project. He understands that desirable results come from hard work. Today, Bobby still carries the same work ethic. Bobby expresses unhappiness to me when he is not achieving goals or doing productive activities. This recently culminated with him enrolling into a full-stack development boot camp, to be pushed into a challenging position where he can learn new engineering skills and pivot his life into a better position. He is driven, amicable, and dependable. I have zero doubt Bobby would be a great developer wherever he goes.",
     },
     {
       id: 2,
-      link: 'https://www.linkedin.com/in/yishak-wesego/',
-      name: 'Yishak Wesego',
-      role: 'Full-stack developer | Technical Support Engineer at Microverse',
-      test: "It was a pleasure collaborating with Meri on different projects. One of the things that I think is special about her is that she never settles, even after completing the projects she finds other resources and strengthens her knowledge. Collaborating with her is easy and comfortable, it's like working with someone you've known for a long period of time.",
-    },
-    {
-      id: 3,
-      link: 'https://www.linkedin.com/in/akuu-khan/',
-      name: 'akbar (Aku) Khan',
-      role: 'Full Stack Developer| Ruby on Rails | PostgreSQL | JavaScript | React | Redux | Html&Css | Python.',
-      test: 'I worked with Meri in the same team and her communication skills are very strong. Her programming skill is one of the bests, given the time frame in the field. She is a good team player.  She will probably fit in most of the companies not only because she\'s a good team worker, but also because she has very good skills and I know she has much more potential to be shown.',
-    },
-    {
-      id: 4,
-      link: 'https://www.linkedin.com/in/isaicespedes/',
-      name: 'Isai Céspedes',
-      role: 'Full-Stack Web Developer. JavaScript | Rails | React | Redux.',
-      test: "I mentored Meri some months ago, and I can say that she is one of those people that you love to work with. She was always interested in what I was trying to teach her, paying attention and always asking questions if something was not clear. When it comes to professional skills, she is really committed to work, always doing her best and going beyond the requirements of the project she's building.",
-    },
-    {
-      id: 5,
-      link: 'https://www.linkedin.com/in/hamzaalitarar/',
-      name: 'Hamza Tarar',
-      role: 'Software Developer',
-      test: "Throughout all our collaborations, Meri has always conducted herself politely and kindly. She comes across as someone that's always willing to help and puts the team ahead of herself. But beneath this, I see a strength and determination to distinguish herself. She's not only someone I highly recommend but is also someone I greatly respect.",
+      link: 'https://www.linkedin.com/in/jake-armijo/',
+      name: 'Jake Armijo',
+      role: 'Software Developer @ Charter Solutions',
+      test: 'Bobby is an easy-going people person. He was always approachable and managed his time effectively. He excelled in back-end development. His knowledge of Node and Express allowed our project to take off fast. He is a great manger and was able to delegate tasks to everyone. He also handled many front end tasks including developing several React components. Bobby has a thorough understanding of Git and how to apply it to your project. He is a detailed oriented person and I would highly recommend him to anyone seeking a team member.',
     },
   ];
   return (
     <section id="testmonials">
       <h5>Feedback from my peers</h5>
       <h2>Testimonials</h2>
-      <Swiper 
+      <Swiper
         className="container testimonials__container"
         modules={[Pagination]}
         spaceBetween={40}
         slidesPerView={1}
         pagination={{ clickable: true }}
-        >
+      >
         {testimonials.map((test) => (
           <SwiperSlide className="testimonial" key={test.id}>
-          <div className="client__avatar">
-            <a href={test.link}>
-              <BsLinkedin />
-            </a>
-          </div>
-          <h5 className='client__name'>{test.name}</h5>
-          <small className="client__review">{test.test}</small>
-        </SwiperSlide>
+            <div className="client__avatar">
+              <a href={test.link}>
+                <BsLinkedin />
+              </a>
+            </div>
+            <h5 className="client__name">{test.name}</h5>
+            <small className="client__review">{test.test}</small>
+          </SwiperSlide>
         ))}
       </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
