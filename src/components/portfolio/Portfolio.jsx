50739a6f6a81db996dfae9f026009e7fcc43bb53
Bobby Ton <bobbyt1997@gmail.com>
2022-08-20 12:46:47 -0500

    update portfolio info

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import IMG1 from '../../assets/bookstore.png';
-import IMG2 from '../../assets/todo.png';
-import IMG3 from '../../assets/shelter.png';
-import IMG4 from '../../assets/fake.png';
-import IMG5 from '../../assets/news.png';
-import IMG6 from '../../assets/math.png';
+import IMG1 from '../../assets/jdb.png';
+import IMG2 from '../../assets/phutbol.png';
 
 import './portfolio.css';
 
@@ -12,62 +8,22 @@ const Portfolio = () => {
   const soloProjects = [
     {
       id: 1,
-      title: 'Bookstore',
+      title: 'JDB Cars',
       img: IMG1,
       description:
-        'BookStore app that allows users to store books and update progress',
-      technologies: 'React | Redux | Ruby on Rails',
-      link: 'https://melodic-boba-111583.netlify.app/',
-      github: 'https://github.com/Meri-MG/bookstore---react',
+        'Web Application that assists users in buying/selling used cars',
+      technologies:
+        'Express | Node | Sequelize | Postgres | React | Redux | Javascript | HTML | CSS',
+      github: 'https://github.com/2006-gs-leo/grace-shopper',
     },
     {
       id: 2,
-      title: 'FakeStore',
-      img: IMG4,
-      description:
-        'FakeStore app that provides information about available products and their details',
-      technologies: 'React | Redux',
-      link: 'https://fakestore-metrics.netlify.app/',
-      github: 'https://github.com/Meri-MG/Fakestore---react',
-    },
-    {
-      id: 3,
-      title: 'To-Do-App',
+      title: 'Phutbol',
       img: IMG2,
-      description: 'User friendly app to plan your day and manage tasks',
-      technologies: 'JavaScript | Webpack',
-      link: 'https://meri-mg.github.io/To-Do-List/dist/',
-      github: 'https://github.com/Meri-MG/To-Do-List',
-    },
-    {
-      id: 4,
-      title: 'Shelter',
-      img: IMG3,
       description:
-        'Fully responsive interactive website built based on Figma design',
-      technologies: 'JavaScript | CSS',
-      link: 'https://meri-mg.github.io/shelter/pages/main/index.html',
-      github: 'https://github.com/Meri-MG/shelter',
-    },
-    {
-      id: 5,
-      title: 'World News',
-      img: IMG5,
-      description:
-        'Fully responsive interactive website built based on Adobe XD design',
-      technologies: 'JavaScript | CSS',
-      link: 'https://meri-mg.github.io/Unilab-world-news/',
-      github: 'https://github.com/Meri-MG/Unilab-world-news',
-    },
-    {
-      id: 6,
-      title: 'Math Resource',
-      img: IMG6,
-      description:
-        'Real-world group project which is still in progress and will provide educational platform for future young developers',
-      technologies: 'JavaScript | Scss | Python',
-      link: 'https://lukinoo.github.io/math-resource/',
-      github: 'https://github.com/lukinoo/math-resource',
+        'Collaborative soccer game inspired by HaxBall aimed at increasing interaction during the pandemic',
+      technologies: 'JavaScript | Phaser | Socket.IO | Firebase',
+      github: 'https://github.com/2006-links-light/phutbol',
     },
   ];
 
@@ -95,14 +51,6 @@ const Portfolio = () => {
                 rel="noreferrer"
               >
                 GitHub
-              </a>
-              <a
-                href={pro.link}
-                target="_blank"
-                className="btn btn-primary"
-                rel="noreferrer"
-              >
-                Live Demo
               </a>
             </div>
           </article>
